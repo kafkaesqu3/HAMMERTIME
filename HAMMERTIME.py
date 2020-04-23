334f1ce755808ea44b3da92e7e5b4e9df15cc722
david <kafkaesque0@protonmail.com>
2020-04-22 20:22:08 -0400

    initial commit: sample JSON config, folder structure

---
+++ HAMMERTIME.py
@@ -0,0 +1,15 @@
+import json
+from stage_builders.shellcode_injection import *
+
+CONFIG = "configs/VIOLETVAGABOND.json"
+
+config_file = open(CONFIG, "r")
+parsed_conf = (json.loads(config_file.read()))
+
+if parsed_conf['stage0']: 
+    stage0 = parsed_conf['stage0']['action']
+    stage0_params = parsed_conf['stage0']['action_params']
+    print("Stage0: {}".format(stage0))
+    if stage0 == "shellcode_injection":
+        stage0_builder = Shellcode_Injection(stage0_params)
+    
---
+++ stage_builders/shellcode_injection.py
@@ -0,0 +1,7 @@
+class Shellcode_Injection: 
+    def __init__(self, args):
+        self.args = args
+        print(self.args)
+
+    
+    
